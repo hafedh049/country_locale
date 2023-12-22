89f22e119e2a55d9ac1a1912267ada22d91b5e06
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-22 17:09:36 +0100

    Added  assets/day.jpg A  assets/night.jpg  M lib/home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -9,21 +9,27 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool _isDay = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/"),
-          ),
+        decoration: const BoxDecoration(
+          image: DecorationImage(image: AssetImage("assets/")),
         ),
         width: MediaQuery.sizeOf(context).width,
         height: MediaQuery.sizeOf(context).height,
         child: Center(
           child: GestureDetector(
             onTap: () {
-              showCountryPicker(context: context, onSelect: (Country value) {});
+              showCountryPicker(
+                context: context,
+                onSelect: (Country value) {
+                  setState(() {
+                    _isDay = !_isDay;
+                  });
+                },
+              );
             },
             child: Container(
               padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
