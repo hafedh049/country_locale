1959abb9ba6a03db2c674b6ff6f6f057c5b76a6a
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-22 17:07:02 +0100

    Modified  lib/home.dart  M pubspec.yaml

--- lib/home.dart
+++ lib/home.dart
@@ -13,11 +13,23 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/"),
+          ),
+        ),
         width: MediaQuery.sizeOf(context).width,
         height: MediaQuery.sizeOf(context).height,
         child: Center(
-          child: Container(
-            child: const Center(child: Text("Pick A Country", style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500))),
+          child: GestureDetector(
+            onTap: () {
+              showCountryPicker(context: context, onSelect: (Country value) {});
+            },
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(15)),
+              child: const Center(child: Text("Pick A Country", style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500))),
+            ),
           ),
         ),
       ),
